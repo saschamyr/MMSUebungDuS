51a65053a569ba811826bd2b48a5dba5b919d6ab
saschamyr <40141870+saschamyr@users.noreply.github.com>
2018-11-01 14:34:03 +0100

    Übung1
    Schau mal ob alles da ist

---
+++ Übung 1/MMS Template/MMS Template/Assets/Scripts/FallTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FallTrigger : MonoBehaviour {
+
+    private float thrust;
+    private Rigidbody rb;
+   
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        thrust = 50.0f;
+    }
+	
+	
+	void Update () {
+        rb.AddForce(transform.forward * thrust);
+    }
+}
